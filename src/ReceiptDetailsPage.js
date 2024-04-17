0acdadff9a75de75e535bb2c4c25c45b175381d3
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-17 10:50:07 +0200

    Update ReceiptDetailsPage.js

--- src/ReceiptDetailsPage.js
+++ src/ReceiptDetailsPage.js
@@ -4,10 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 function ReceiptDetailsPage({ receipts }) {
   return (
     <div className="container">
-      <h1 className="text-center">Receipt Details</h1>
+      <h1 className="text-center text-primary">Receipt Details</h1>
       <div className="row justify-content-center">
         <div className="col-md-10">
-          <table className="table">
+          <table className="table table-bordered table-striped">
             <thead className="thead-dark">
           <tr>
             <th>Timestamp</th>
