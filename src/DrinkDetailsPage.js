362f2baee24c730ac5f3ad63c3c26741d80d0679
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-19 23:46:00 +0200

    Update DrinkDetailsPage.js

--- src/DrinkDetailsPage.js
+++ src/DrinkDetailsPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-
+import { Link } from 'react-router-dom'; // Import Bootstrap components
 
 function DrinkDetailsPage({ drinks }) {
   return (
@@ -8,12 +8,13 @@ function DrinkDetailsPage({ drinks }) {
       <h1 className="text-center text-primary">Drink Details</h1>
       <div className="row justify-content-center">
         <div className="col-md-10">
-          <table className="table table-bordered table-striped">
+          <table className="table table-bordered table-striped ">
             <thead className="thead-dark">
           <tr>
             <th>Name</th>
             <th>Type</th>
             <th>Quantity</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -22,6 +23,17 @@ function DrinkDetailsPage({ drinks }) {
               <td>{drink.name}</td>
               <td>{drink.type}</td>
               <td>{drink.quantity}</td>
+              <td>
+                
+                <button>
+                  <a>✏️</a> 
+                </button>
+                <a> </a>
+                <button>
+                  <a>❌</a> 
+                </button>
+                
+              </td>
             </tr>
           ))}
         </tbody>
