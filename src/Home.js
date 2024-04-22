1c329981824c8f4d40057a936d3e97dca98d714c
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-22 14:13:18 +0200

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="text-center mt-5">
+        <h1 className="display-3">Welcome to Shop24</h1>
+        <p className="lead">Your one-stop solution for soft drinks wholesale.</p>
+      </div>
+      <div className="mt-5">
+        <h2 className="text-primary">Introduction to Shop24</h2>
+        <p>Shop24 is a soft drinks wholesaler based in Kigali. They aim to provide a white-label solution to existing retailer companies/hotels by building a set of APIs. These APIs will allow hotels to manage their requests for buying from Shop24 and transporting the goods to their clients. Your task is to help build these APIs and document them using Swagger.</p><br></br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
