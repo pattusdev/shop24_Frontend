dce6f96eaf094669dc374537bf4530206cda0e8f
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-22 14:13:30 +0200

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-light">
+      <div className="container text-center">
+        
+        <span className="text-muted">Shop24 &copy;2024</span>
+        <br></br>
+        <br></br>
+        <br></br>
+        
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
