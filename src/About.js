7c6fa508eb497188eaf158b6f1625d7107e12a5b
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-25 09:18:19 +0200

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="container">
+      <div className="text-center mt-5">
+        <h1 className="display-3">About Shop24</h1>
+        <p className="lead">Learn more about who we are and what we do.</p>
+      </div>
+      <div className="mt-5">
+        <h2 className="text-primary">Who is Shop24?</h2>
+        <p>Sollicitudin ac orci phasellus egestas tellus 
+          rutrum tellus pellentesque eu. Sed vulputate odio ut enim blandit. 
+          Blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque. 
+          Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. 
+          Dolor sit amet consectetur adipiscing elit. Id leo in vitae turpis massa sed elementum tempus. A arcu cursus vitae congue mauris rhoncus aenean vel. Mauris ultrices eros in cursus turpis massa tincidunt. Nec nam aliquam sem et. Vulputate sapien nec sagittis aliquam malesuada bibendum. Lorem donec massa sapien faucibus. Augue interdum velit euismod in pellentesque massa placerat. Pellentesque habitant morbi tristique senectus et netus et malesuada. Nam at lectus urna duis convallis convallis tellus id interdum. Nisi scelerisque eu ultrices vitae auctor. Lorem mollis aliquam ut porttitor leo. Hac habitasse platea dictumst vestibulum. Pellentesque habitant morbi tristique senectus et. Turpis massa sed elementum tempus egestas sed sed. Pharetra vel turpis nunc eget lorem dolor sed.</p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
