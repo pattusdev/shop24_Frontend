caa88e8f0222f724e7b2ce60be6a20291f634f71
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-04-30 18:09:09 +0200

    Update ClientDetailsPage.js

--- src/ClientDetailsPage.js
+++ src/ClientDetailsPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import { Trash } from 'react-bootstrap-icons';
 import { PencilSquare } from 'react-bootstrap-icons';
-import DashBoard from './Dashboard';
+import DashBoard from './Dashboard'; // Import Dashboard.js
 
 function ClientDetailsPage() {
   const [clients, setClients] = useState([]);
