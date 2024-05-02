86394ee535b93b3fbd8e45d17b2bdd6f2e246c9f
Rwego Patrick <rwego.patrick.50@gmail.com>
2024-05-02 15:29:23 +0200

    Update Navigation.js

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // Import Link from React Router Dom
 
 function Navigation() {
   return (
